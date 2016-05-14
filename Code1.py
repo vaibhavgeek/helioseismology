87ccbd5495c26abb39d9357560f12270e0f15974
aniruddhadeshmukh <aniruddha480@gmail.com>
2016-05-15 00:04:33 +0530

    Add files via upload

---
+++ Code1.py
@@ -0,0 +1,7 @@
+import numpy as np			#create object of numpy library
+import pylab as pl			#create object of pylab library
+data = np.loadtxt('Data.txt')		#loading the data in text format into Python. The file should be in same folder as this code.
+pl.plot(data[:,1], data[:,3])		#The numbers represent the column numbers in our data which are to be plotted on x- and y- axis 					respectively
+pl.xlabel('l')				#Labelling the x- axis by variable plotted on it	
+pl.ylabel('nu')				#Labelling the y- axis by variable plotted on it
+pl.show()				#Using show function to create an image of graph plotted in Python
