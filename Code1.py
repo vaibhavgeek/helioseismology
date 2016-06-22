9c2b8cb853857cc8577c74e78054efcbd5c889ee
Vaibhu9996 <vaibhu_bond007@yahoo.co.in>
2016-06-22 20:39:17 +0530

    Changed data file name

--- Code1.py
+++ Code1.py
@@ -1,6 +1,6 @@
 import numpy as np			#create object of numpy library
 import pylab as pl			#create object of pylab library
-data = np.loadtxt('Data.txt')		#loading the data in text format into Python. The file should be in same folder as this code.
+data = np.loadtxt('data.txt')		#loading the data in text format into Python. The file should be in same folder as this code.
 pl.plot(data[:,1], data[:,3])		#The numbers represent the column numbers in our data which are to be plotted on x- and y- axis 					respectively
 pl.xlabel('l')				#Labelling the x- axis by variable plotted on it	
 pl.ylabel('nu')				#Labelling the y- axis by variable plotted on it
